8baa1315a51cde6161f0bf372e1b9ce91e48ec55
Ariel Armijo Q <arielarmijo@yahoo.es>
2020-09-07 15:31:35 -0300

    [M3A16] Refactoriza código y agrega identificación de butacas

--- mod3/act16/js/app.js
+++ mod3/act16/js/app.js
@@ -1,12 +1,24 @@
+// Películas disponibles
+var peliculas = [
+    { id: 1, nombre: "Last Shot", precio: 4500 },
+    { id: 2, nombre: "Still Here", precio: 3500 },
+    { id: 3, nombre: "Will Reading", precio: 2500 },
+    { id: 4, nombre: "The Pale Door", precio: 5500 },
+    { id: 5, nombre: "Deep Blue Sea 3D", precio: 7500 }
+];
+
+var precio = peliculas[0].precio;
+var filas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+var ultimaFila = filas.length;
+var ultimaButaca = 14;
+var asientos = [];
+
+var fmt = new Intl.NumberFormat('es-CL', {
+    style: 'currency',
+    currency: 'CLP'
+});
+
 window.addEventListener("load", function () {
-    // Películas disponibles
-    var peliculas = [
-        { id: 1, nombre: "Last Shot", precio: 4500 },
-        { id: 2, nombre: "Still Here", precio: 3500 },
-        { id: 3, nombre: "Will Reading", precio: 2500 },
-        { id: 4, nombre: "The Pale Door", precio: 5500 },
-        { id: 5, nombre: "Deep Blue Sea 3D", precio: 7500 }
-    ];
 
     // Llena el select con opciones de películas
     var $peliculas = document.getElementById("peliculas");
@@ -18,37 +30,29 @@ window.addEventListener("load", function () {
     });
 
     // Muestra el precio de la película
-    var precio = peliculas[0].precio;
     var $precio = document.getElementById("precio");
-    $precio.textContent = precio;
+    $precio.textContent = fmt.format(precio);
     $peliculas.addEventListener("change", function () {
         var peliculaSeleccionada = peliculas.find(p => p.id == this.value);
         precio = peliculaSeleccionada.precio;
-        $precio.textContent = precio;
+        $precio.textContent = fmt.format(precio);
     });
 
-    var $numeroAsientos = document.getElementById("numeroAsientos");
-    $numeroAsientos.textContent = 0;
-    var $precioTotal = document.getElementById("precioTotal");
-    $precioTotal.textContent = 0;
-
-    var filas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
-    var ultimaFila = filas.length;
+    // Crea las butacas del cine
     var $butacas = document.querySelector("#butacas");
-    var ultimaButaca = 12;
-
-    var asientos = [];
     var $asientos = document.getElementById("asientos");
 
     for (let i = 0; i < ultimaFila; i++) {
         var fila = document.createElement("div");
         fila.id = filas[i];
         fila.classList.add("fila");
+        fila.textContent = filas[i];
         $butacas.append(fila);
         for (let j = 0; j < ultimaButaca; j++) {
             var butaca = document.createElement("div");
             butaca.id = `${filas[i]}${j}`;
             butaca.classList.add("butaca");
+            butaca.textContent = j + 1;
             butaca.addEventListener("click", function () {
                 console.log(this.id);
                 this.classList.toggle("seleccionado");
@@ -59,19 +63,24 @@ window.addEventListener("load", function () {
                         asientos.pop(this.id);
                 }
                 $numeroAsientos.textContent = asientos.length;
-                $asientos.textContent = asientos.toString();
-                $precioTotal.textContent = asientos.length * precio;
+                $asientos.textContent = asientos.length > 0 ? "(" + asientos.toString() + ")" : "";
+                $precioTotal.textContent = fmt.format(asientos.length * precio);
             });
             fila.append(butaca);
         }
     }
 
+    // Muestra información sobre la venta
+    var $numeroAsientos = document.getElementById("numeroAsientos");
+    $numeroAsientos.textContent = 0;
+    var $precioTotal = document.getElementById("precioTotal");
+    $precioTotal.textContent = 0;
+
+    // Acción del botón pagar
     var $btnPagar = document.getElementById("btnPagar");
     $btnPagar.addEventListener("click", function () {
         console.log(asientos);
-        asientos.forEach(a => {
-            document.getElementById(a).classList.add("ocupado");
-        });
+        asientos.forEach(a => { document.getElementById(a).classList.add("ocupado"); });
         asientos = [];
         $numeroAsientos.textContent = 0;
         $asientos.textContent = "";
